b2c73db8d4b04ee388661b6d1373ef4017291327
Alexandr <kobal2009@yandex.ru>
2023-01-15 22:55:11 +0500

    Усложненное задание к уроку 3

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 'use strict'
 
-const func = (str) => {
-    str = prompt('Введите что нибудь', '');
-    str = str.trim();
-        if (!isNaN(str)) {
-        console.log("Это не строка!")
-    } else if (str.length > 30) {console.log(str.slice(0, 30) + '...')
-    } else {
-        console.log(str)
-    }
-  }
-func()
+
+const daysRu = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"]
+const daysEn = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", " Friday", "Saturday"]
+let myDate = new Date()
+
+
+let dateRu = "Сегодня: " + daysRu[myDate.getDay()]
+let dateEn = "Today is: " + daysEn[myDate.getDay()]
+
+let lang = prompt("ru or en?")
+let namePerson = prompt("Введите имя")
+
+
+let status = namePerson === "Артем" ? console.log("директор") : namePerson === "Александр" ? console.log("преподаватель") : console.log("студент")
+let ourDate = lang === "ru" ? console.log(dateRu) : lang === "en" ? console.log(dateEn) : console.log("Ошибка")
+
+
+
+
+if (lang === "ru") {
+  console.log(dateRu)
+} else if (lang === "en") {
+  console.log(dateEn)
+} else {
+  console.log("Ошибка")
+}
+
+switch (true) {
+  case lang === "ru":
+    console.log(dateRu)
+    break;
+  case lang === "en":
+    console.log(dateEn)
+    break;
+  default:
+    console.log("Ошибка")
+}
+
+
